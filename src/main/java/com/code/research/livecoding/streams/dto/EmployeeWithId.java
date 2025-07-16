47003058c1f45605b6ea274e6a425a705753af95
diafter <diafter@gmail.com>
2025-07-16 19:26:48 +0200

    Streams algorithms

---
+++ src/main/java/com/code/research/livecoding/streams/dto/EmployeeWithId.java
@@ -0,0 +1,20 @@
+package com.code.research.livecoding.streams.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+public class EmployeeWithId {
+
+    private final Long id;
+    private final String name;
+
+    public EmployeeWithId(Long id, String name) {
+        this.id   = Objects.requireNonNull(id,   "id must not be null");
+        this.name = Objects.requireNonNull(name, "name must not be null");
+    }
+
+}
