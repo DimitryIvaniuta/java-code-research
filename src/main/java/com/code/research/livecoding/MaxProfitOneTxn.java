7ae79da04a21ad3f3a1df639cb0e6db14a9b84a3
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2025-10-19 18:54:19 +0200

    Max Profit algorithm

---
+++ src/main/java/com/code/research/livecoding/MaxProfitOneTxn.java
@@ -0,0 +1,24 @@
+package com.code.research.livecoding;
+
+public class MaxProfitOneTxn {
+    // Returns max profit from exactly one buy and one sell (buy before sell).
+    public static int maxProfit(int[] prices) {
+        int minProfit = Integer.MAX_VALUE;
+        int bestProfit = 0;
+        for (int p : prices) {
+            if (p < minProfit) {
+                minProfit = p;
+            }
+            int profit = p - minProfit;
+            if (profit > bestProfit) {
+                bestProfit = profit;
+            }
+        }
+        return bestProfit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4})); // 5 (buy 1, sell 6)
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));   // 0 (no profit)
+    }
+}
