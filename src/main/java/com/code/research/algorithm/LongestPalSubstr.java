dab5136985c5d767dbc65f2a21521a13417da80c
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2025-11-23 22:50:05 +0100

    Longest Palindromic Substring Algorithm

--- src/main/java/com/code/research/algorithm/LongestPalSubstr.java
+++ src/main/java/com/code/research/algorithm/LongestPalSubstr.java
@@ -1,34 +1,46 @@
 package com.code.research.algorithm;
 
-public final class LongestPalSubstr {                       // Final utility class for longest palindromic substring
-    public static String longestPalindrome(String s) {      // Entry method: returns the longest palindromic substring of s
-        int n = s.length(), L = 0, R = 0;                   // n = length; [L,R] stores best palindrome bounds (inclusive)
-
-        for (int i = 0; i < n; i++) {                       // Try every index as a center
-            int l = i, r = i;                               // Odd-length center at i (single character center)
-            while (l >= 0 && r < n &&                       // Expand while in bounds
-                    s.charAt(l) == s.charAt(r)) {            // and characters match
-                if (r - l > R - L) {
-                    L = l;
-                    R = r;
-                }        // Update best bounds if this palindrome is longer
+public final class LongestPalSubstr {
+    // Final utility class for longest palindromic substring
+    public static String longestPalindrome(String s) {
+        // Entry method: returns the longest palindromic substring of s
+        // n = length; [l, r] store best palindrome bounds (inclusive)
+        int n = s.length();
+        int lBound = 0;
+        int rBound = 0;
+        // Try every index as a center
+        for (int i = 0; i < n; i++) {
+            // Odd-length center at i (single character center)
+            int l = i;
+            int r = i;
+            // Expand while in bounds
+            // and characters match
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                if (r - l > rBound - lBound) {
+                    // Update best bounds if this palindrome is longer
+                    lBound = l;
+                    rBound = r;
+                }
                 l--;
                 r++;                                   // Expand outward
             }
 
             l = i;
-            r = i + 1;                               // Even-length center between i and i+1
-            while (l >= 0 && r < n &&                       // Expand while in bounds
-                    s.charAt(l) == s.charAt(r)) {            // and characters match
-                if (r - l > R - L) {
-                    L = l;
-                    R = r;
-                }        // Update best bounds if longer
+            // Even-length center between i and i+1
+            r = i + 1;
+            // and characters match
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                if (r - l > rBound - lBound) {
+                    // Update best bounds if longer
+                    lBound = l;
+                    rBound = r;
+                }
+                // Expand outward
                 l--;
-                r++;                                   // Expand outward
+                r++;
             }
         }
-
-        return s.substring(L, R + 1);                       // Extract longest palindrome using inclusive [L,R]
-    }                                                       // End of longestPalindrome
-}                                                           // End of class
+        // Extract longest palindrome using inclusive [L,R]
+        return s.substring(lBound, rBound + 1);
+    }
+}
