fef7eb5b6c698d03a752f6d7916c6a2aca963ec1
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2025-11-24 20:05:54 +0100

    LRUCache algorithm

---
+++ src/main/java/com/code/research/algorithm/LRUCache.java
@@ -0,0 +1,54 @@
+package com.code.research.algorithm;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public final class LRUCache {
+
+    private int cap;
+    private Map<Integer, Integer> map;
+    private ReentrantReadWriteLock lock;
+    public LRUCache(int capacity) {
+        this.cap = capacity;
+        this.lock = new ReentrantReadWriteLock();
+        this.map = new LinkedHashMap<>(14, .75f, true);
+    }
+
+    public LRUCache(int capacity, boolean b2) {
+        this.cap = capacity;
+        this.map = new LinkedHashMap<>(16, .75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> e) {
+                return size() > cap;
+            }
+        };
+
+    }
+
+    public int get(int key) {
+        lock.readLock().lock();
+        Integer v = map.get(key);
+        try{
+            return v == null ? -1 : v;
+        } finally {
+            lock.readLock().unlock();
+        }
+
+    }
+
+    public void put(int key, int value) {
+        lock.writeLock().lock();
+        try{
+            map.put(key, value);
+            if(map.size() > cap){
+                Iterator<Map.Entry<Integer, Integer>> it = map.entrySet().iterator();
+                it.next();
+                it.remove();
+            }
+        } finally {
+            lock.writeLock().unlock();
+        }
+
+    }
+}
